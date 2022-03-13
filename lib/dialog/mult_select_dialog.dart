9f687926127286ba739740fb1f0c4d90d53c1002
kevmiclee12 <87021378+kevmiclee12@users.noreply.github.com>
2022-03-12 21:47:42 -0500

    Update mult_select_dialog.dart

--- lib/dialog/mult_select_dialog.dart
+++ lib/dialog/mult_select_dialog.dart
@@ -236,22 +236,36 @@ class _MultiSelectDialogState<V> extends State<MultiSelectDialog<V>> {
             ),
           ),
           Container(
-            color: widget.isDark ? kDarkBackgroundColor100 : Colors.white,
+            color: widget.isDark ? Color(0xff242A37) : Colors.white,
             margin: const EdgeInsets.only(right: 8, bottom: 8),
-            child: CustomCardShadow(
+            child: CardShadow(
               noShadow: true,
               cardHeight: 48,
               cardWidth: MediaQuery.of(context).size.width,
               radius: 8,
               isDark: widget.isDark,
-              darkModeColor: kDarkBackgroundColor,
+              darkModeColor: Color(0xffF8F9FC),
               child: CustomPaint(
-                painter: CustomCardBorderGradientPainter(
+                painter: CardBorderGradientPainter(
                   strokeWidth: 1,
                   radius: 8,
                   gradient: widget.isDark
-                      ? kDarkBorderGradientVertical
-                      : kBorderGradientVertical,
+                      ? LinearGradient(
+  begin: Alignment.topCenter,
+  end: Alignment.bottomCenter,
+  colors: [
+    Color(0xff40ACD7),
+    Color(0xffB770CE),
+  ],
+)
+                      : LinearGradient(
+  begin: Alignment.topCenter,
+  end: Alignment.bottomCenter,
+  colors: [
+    Color(0xffAEF3FF),
+    Color(0xff8840A0),
+  ],
+),
                 ),
                 child: Container(
                   padding: const EdgeInsets.only(left: 17, right: 17),
@@ -271,11 +285,11 @@ class _MultiSelectDialogState<V> extends State<MultiSelectDialog<V>> {
                           controller: widget.controller,
                           style: TextStyle(
                             color: widget.isDark
-                                ? kDarkThemeTextColor
-                                : kTextColor,
+                                ? Color(0xffF9FBFF)
+                                : Color(0xff202531),
                           ),
                           cursorColor:
-                              widget.isDark ? kDarkThemeGrey : kMainColor,
+                              widget.isDark ? Color(0xff6C768E) : Color(0xff692B7E),
                           textCapitalization: TextCapitalization.sentences,
                           decoration: InputDecoration(
                             contentPadding: EdgeInsets.zero,
@@ -288,28 +302,14 @@ class _MultiSelectDialogState<V> extends State<MultiSelectDialog<V>> {
                                 ///update value to ''
                               },
                               child: widget.controller.text == ''
-                                  ? CustomIcon(
-                                      iconName: 'Search',
-                                      isDark: widget.isDark,
-                                      size: 22,
-                                      color: widget.isDark
-                                          ? kDarkThemeTextColor200
-                                          : kTextColor200,
-                                    )
-                                  : CustomIcon(
-                                      iconName: 'Close',
-                                      isDark: widget.isDark,
-                                      size: 22,
-                                      color: widget.isDark
-                                          ? kDarkThemeTextColor200
-                                          : kTextColor200,
-                                    ),
+                                  ? widget.searchIcon
+                                  : widget.closeSearchIcon,
                             ),
                             hintText: widget.hintText,
                             hintStyle: TextStyle(
                               color: widget.isDark
-                                  ? kDarkThemeTextColor200
-                                  : kTextColor300,
+                                  ? Color(0xff657393)
+                                  : Color(0xff515D7A),
                               fontSize: 14,
                             ),
                             border: const OutlineInputBorder(
@@ -387,7 +387,7 @@ class _MultiSelectDialogState<V> extends State<MultiSelectDialog<V>> {
             Center(
               child: Container(
                 margin: const EdgeInsets.only(top: 16, bottom: 16),
-                child: CustomDefaultButton(
+                child: DefaultButton(
                   isDark: widget.isDark,
                   title: 'widget.buttonText',
                   width: MediaQuery.of(context).size.width / 1.5,
@@ -405,116 +405,214 @@ class _MultiSelectDialogState<V> extends State<MultiSelectDialog<V>> {
   }
 }
 
-// class CustomDefaultButton extends StatelessWidget {
-//   final bool isDark;
-//   final String title;
-//   final VoidCallback onPressed;
-//   final bool disabled;
-//   final LinearGradient colorGradient;
-//   final Color pressedColor;
-//   final double height;
-//   final double? width;
-//   final bool customContent;
-//   final Widget? customContentWidget;
-//   final double borderRadius;
-//   final bool shrinkText;
-//   final Color? changeColor;
-//   final double? padding;
-//   final bool noShadow;
+class DefaultButton extends StatelessWidget {
+  final bool isDark;
+  final String title;
+  final VoidCallback onPressed;
+  final bool disabled;
+  final LinearGradient colorGradient;
+  final Color pressedColor;
+  final double height;
+  final double? width;
+  final bool customContent;
+  final Widget? customContentWidget;
+  final double borderRadius;
+  final bool shrinkText;
+  final Color? changeColor;
+  final double? padding;
+  final bool noShadow;
 
-//   const CustomDefaultButton({
-//     Key? key,
-//     Color pressedColor = const Color(0xff692B7E),
-//     required this.isDark,
-//     required this.title,
-//     required this.onPressed,
-//     this.customContentWidget,
-//     this.changeColor,
-//     this.padding,
-//     this.disabled = false,
-//     LinearGradient? colorGradient,
-//     this.height = 48,
-//     this.width,
-//     this.customContent = false,
-//     this.borderRadius = 10,
-//     this.shrinkText = false,
-//     this.noShadow = false,
-//   })  : colorGradient = const LinearGradient(
-//   begin: Alignment(0, -2),
-//   end: Alignment(-1, 2),
-//   colors: [Color(0xff692B7E), Color(0xff16061E)],
-// ),
-//         pressedColor = isDark ? const Color(0xff9E41BD) : const Color(0xff692B7E),
-//         super(key: key);
+  const DefaultButton({
+    Key? key,
+    Color pressedColor = const Color(0xff692B7E),
+    required this.isDark,
+    required this.title,
+    required this.onPressed,
+    this.customContentWidget,
+    this.changeColor,
+    this.padding,
+    this.disabled = false,
+    LinearGradient? colorGradient,
+    this.height = 48,
+    this.width,
+    this.customContent = false,
+    this.borderRadius = 10,
+    this.shrinkText = false,
+    this.noShadow = false,
+  })  : colorGradient = const LinearGradient(
+  begin: Alignment(0, -2),
+  end: Alignment(-1, 2),
+  colors: [Color(0xff692B7E), Color(0xff16061E)],
+),
+        pressedColor = isDark ? const Color(0xff9E41BD) : const Color(0xff692B7E),
+        super(key: key);
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       height: height,
-//       width: width,
-//       decoration: disabled
-//           ? const BoxDecoration(color: Colors.transparent)
-//           : BoxDecoration(
-//               gradient: changeColor != null
-//                   ? LinearGradient(colors: [changeColor!, changeColor!])
-//                   : isDark
-//                       ? LinearGradient(
-//   begin: Alignment(-2, -1),
-//   end: Alignment(1, 1),
-//   colors: [Color(0xff7D4092), Color(0xffE288FF)],
-// )
-//                       : colorGradient,
-//               borderRadius: BorderRadius.circular(borderRadius),
-//             ),
-//       child: ElevatedButton(
-//         onPressed: onPressed,
-//         style: ButtonStyle(
-//           shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-//             RoundedRectangleBorder(
-//               borderRadius: BorderRadius.circular(borderRadius),
-//             ),
-//           ),
-//           backgroundColor: MaterialStateProperty.resolveWith<Color>(
-//             (Set<MaterialState> states) {
-//               if (states.contains(MaterialState.pressed)) {
-//                 return disabled ? Color(0xff6C768E) : pressedColor;
-//               }
-//               return disabled ? Color(0xff6C768E) : Colors.transparent;
-//             },
-//           ),
-//           elevation: MaterialStateProperty.resolveWith<double>(
-//             (Set<MaterialState> states) {
-//               if (states.contains(MaterialState.pressed)) return 0.0;
-//               return 8.0;
-//             },
-//           ),
-//           shadowColor: MaterialStateProperty.all(
-//             disabled || noShadow
-//                 ? Colors.transparent
-//                 : isDark
-//                     ? const Color.fromRGBO(197, 103, 229, 0.18)
-//                     : const Color.fromRGBO(83, 32, 100, 0.3),
-//           ),
-//           splashFactory: NoSplash.splashFactory,
-//           padding: MaterialStateProperty.all(
-//               padding != null ? EdgeInsets.all(padding!) : null),
-//         ),
-//         child: customContent
-//             ? customContentWidget
-//             : AutoSizeText(
-//                 title,
-//                 style: TextStyle(
-//                   fontFamily: 'Mont',
-//                   fontWeight: FontWeight.w700,
-//                   color: isDark && disabled
-//                       ? Color(0xffF9FBFF)
-//                       : isDark
-//                           ? Color(0xff202531)
-//                           : Colors.white,
-//                   fontSize: shrinkText ? 11.4 : 14,
-//                 ),
-//               ),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: height,
+      width: width,
+      decoration: disabled
+          ? const BoxDecoration(color: Colors.transparent)
+          : BoxDecoration(
+              gradient: changeColor != null
+                  ? LinearGradient(colors: [changeColor!, changeColor!])
+                  : isDark
+                      ? LinearGradient(
+  begin: Alignment(-2, -1),
+  end: Alignment(1, 1),
+  colors: [Color(0xff7D4092), Color(0xffE288FF)],
+)
+                      : colorGradient,
+              borderRadius: BorderRadius.circular(borderRadius),
+            ),
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ButtonStyle(
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(borderRadius),
+            ),
+          ),
+          backgroundColor: MaterialStateProperty.resolveWith<Color>(
+            (Set<MaterialState> states) {
+              if (states.contains(MaterialState.pressed)) {
+                return disabled ? Color(0xff6C768E) : pressedColor;
+              }
+              return disabled ? Color(0xff6C768E) : Colors.transparent;
+            },
+          ),
+          elevation: MaterialStateProperty.resolveWith<double>(
+            (Set<MaterialState> states) {
+              if (states.contains(MaterialState.pressed)) return 0.0;
+              return 8.0;
+            },
+          ),
+          shadowColor: MaterialStateProperty.all(
+            disabled || noShadow
+                ? Colors.transparent
+                : isDark
+                    ? const Color.fromRGBO(197, 103, 229, 0.18)
+                    : const Color.fromRGBO(83, 32, 100, 0.3),
+          ),
+          splashFactory: NoSplash.splashFactory,
+          padding: MaterialStateProperty.all(
+              padding != null ? EdgeInsets.all(padding!) : null),
+        ),
+        child: customContent
+            ? customContentWidget
+            : AutoSizeText(
+                title,
+                style: TextStyle(
+                  fontFamily: 'Mont',
+                  fontWeight: FontWeight.w700,
+                  color: isDark && disabled
+                      ? Color(0xffF9FBFF)
+                      : isDark
+                          ? Color(0xff202531)
+                          : Colors.white,
+                  fontSize: shrinkText ? 11.4 : 14,
+                ),
+              ),
+      ),
+    );
+  }
+}
+
+class CardShadow extends StatelessWidget {
+  final double? cardHeight;
+  final double? cardWidth;
+  final double radius;
+  final bool isDark;
+  final Widget child;
+  final bool noShadow;
+  final Color darkModeColor;
+
+  const CardShadow({
+    Key? key,
+    required this.isDark,
+    this.cardHeight,
+    this.cardWidth,
+    required this.radius,
+    required this.child,
+    this.noShadow = false,
+    this.darkModeColor = Color(0xff1C212B),
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          height: cardHeight,
+          width: cardWidth,
+          decoration: BoxDecoration(
+            color: isDark ? darkModeColor : Colors.white,
+            boxShadow: noShadow
+                ? []
+                : [isDark ? BoxShadow(
+  color: Color.fromRGBO(0, 0, 0, 0.13),
+  offset: Offset(4, 4),
+  blurRadius: 12,
+) : BoxShadow(
+  color: Color.fromRGBO(121, 98, 249, 0.13),
+  offset: Offset(2, 4),
+  blurRadius: 12,
+)],
+            borderRadius: BorderRadius.circular(radius),
+          ),
+          child: Container(),
+        ),
+        child,
+      ],
+    );
+  }
+}
+
+class CardBorderGradientPainter extends CustomPainter {
+  final Paint _paint = Paint()..color = Colors.white;
+  final double radius;
+  final double strokeWidth;
+  final Gradient gradient;
+  final bool bold;
+  final bool disabled;
+
+  CardBorderGradientPainter({
+    required this.strokeWidth,
+    required this.radius,
+    required this.gradient,
+    this.bold = false,
+    this.disabled = false,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    // create outer rectangle equals size
+    Rect outerRect = Offset.zero & size;
+    var outerRRect =
+        RRect.fromRectAndRadius(outerRect, Radius.circular(radius));
+
+    // create inner rectangle smaller by strokeWidth
+    Rect innerRect = Rect.fromLTWH(strokeWidth, strokeWidth,
+        size.width - strokeWidth * 2, size.height - strokeWidth * 2);
+    var innerRRect = RRect.fromRectAndRadius(
+        innerRect, Radius.circular(radius - strokeWidth));
+
+    // apply gradient shader
+    if (!bold || disabled) {
+      _paint.shader = gradient.createShader(outerRect);
+    }
+
+    // create difference between outer and inner paths and draw it
+    Path path1 = Path()..addRRect(outerRRect);
+    Path path2 = Path()..addRRect(innerRRect);
+    var path = Path.combine(PathOperation.difference, path1, path2);
+    // canvas.rotate(math.pi / 2);
+    canvas.drawPath(path, _paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => oldDelegate != this;
+}
+
+
