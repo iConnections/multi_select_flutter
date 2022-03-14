33ca7aeafe170b9582fe34637695d2b0771bf23c
kevmiclee12 <87021378+kevmiclee12@users.noreply.github.com>
2022-03-14 12:17:58 -0400

    Update multi_select_item.dart

--- lib/util/multi_select_item.dart
+++ lib/util/multi_select_item.dart
@@ -1,7 +1,9 @@
 /// A model class used to represent a selectable item.
 class MultiSelectItem<V> {
-  const MultiSelectItem(this.value, this.label);
+  const MultiSelectItem(this.value, this.label, this.imageUrl, this.initials);
 
   final V value;
   final String label;
+  final String imageUrl;
+  final String initials;
 }
